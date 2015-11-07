f29e26df4d5a6089cc26fcda975a7db8d664c21d
Kiddos <josephyu831007@yahoo.com.tw>
2015-11-08 01:50:33 +0800

    show password for registration

--- js/register.js
+++ js/register.js
@@ -6,6 +6,10 @@ function hideText(text) {
   return hiddenText;
 }
 
+String.prototype.insert = function(text, pos) {
+  return this.substring(0, pos) + text + this.substring(pos);
+};
+
 var password = document.getElementById('password');
 var confirmPw = document.getElementById('confirm');
 var cbshow = document.getElementById('show');
@@ -22,26 +26,37 @@ function checkStatus() {
   }
 }
 
-//checkStatus();
-
 $(cbshow).on('click', function() {
   //checkStatus();
 });
 
+var pwpos = 0;
 $(password).on('keypress', function(event) {
-  console.log(event.which);
-  switch (event.which) {
-  }
-  passwordCache += String.fromCodePoint(event.which);
-  console.log($(this).caret().start);
+  //console.log(event.which);
+  var ch = String.fromCharCode(event.which);
+  pwpos = $(this).caret();
+
+  passwordCache = passwordCache.insert(ch, pwpos);
 });
 
 $(password).on('input', function() {
   passwordCache = passwordCache.substr(0, password.value.length);
   password.value = hideText(passwordCache);
+  $(this).caret(pwpos+1);
+});
+
+var cfpos = 0;
+$(confirmPw).on('keypress', function(event) {
+  var ch = String.fromCharCode(event.which);
+  cfpos = $(this).caret();
+
+  confirmPwCache = confirmPwCache.insert(ch, cfpos);
+  console.log(confirmPwCache);
 });
 
 $(confirmPw).on('input', function() {
-  console.log('input');
-  //checkStatus();
+  confirmPwCache = confirmPwCache.substr(0, confirmPw.value.length);
+  confirmPw.value = hideText(confirmPwCache);
+  $(this).caret(cfpos+1);
 });
+
